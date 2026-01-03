980432cde768122efa5618689c988ad48fe68d81
kagisogaphane <kagisogift291@outlook.com>
2026-01-03 13:07:10 +0200

    Did code clean up

--- Record Book/Views/Main.xaml.cs
+++ Record Book/Views/Main.xaml.cs
@@ -1,16 +1,7 @@
 ﻿using Record_Book.Models;
 using Record_Book.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Record_Book.Views
 {
